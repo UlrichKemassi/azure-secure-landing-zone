c11a77a9464087e366c48623e68cf4532388fb6a
UlrichKemassi <kemassiulrich.itm@gmail.com>
2026-03-15 05:13:53 -0400

    Update Terraform_Network.tf

--- Terraform_Network.tf
+++ Terraform_Network.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "hub_vnet" {
    name                  = var.hub_vnet_name
    location              = azurerm_resource_group.rg.location
-   resource_group_name   = azure_resource_group.rg.name
+   resource_group_name   = azurerm_resource_group.rg.name
    address_space         = var.hub_vnet_address_space
 }
 
@@ -12,9 +12,44 @@ resource "azurerm_subnet" "azure_bastion_subnet" {
     address_prefixes        = [var.azure_bastion_subnet_cidr]
 }
 
-resource "azurerm_suubnet" "shared_services_subnet" {
+resource "azurerm_subnet" "shared_services_subnet" {
   name                 = "sharedServicesSubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.hub_vnet.name
-  address_prefixes     = [var.shared_services_subnet-cidr]
+  address_prefixes     = [var.shared_services_subnet_cidr]
+}
+
+resource "azurerm_virtual_network" "production_spoke_vnet" {
+   name                  = var.production_spoke_vnet_name
+   location              = azurerm_resource_group.rg.location
+   resource_group_name   = azurerm_resource_group.rg.name
+   address_space         = var.production_spoke_vnet_address_space
+}
+
+resource "azurerm_subnet" "app_subnet" {
+    name                    = "AppsSubnet"
+    resource_group_name     = azurerm_resource_group.rg.name
+    virtual_network_name    = azurerm_virtual_network.production_spoke_vnet.name
+    address_prefixes        = [var.app_subnet_cidr]
+}
+
+resource "azurerm_subnet" "data_subnet" {
+  name                 = "DataSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.production_spoke_vnet.name
+  address_prefixes     = [var.data_subnet_cidr]
+}
+
+resource "azurerm_virtual_network" "management_spoke_vnet" {
+   name                  = var.management_spoke_vnet_name
+   location              = azurerm_resource_group.rg.location
+   resource_group_name   = azurerm_resource_group.rg.name
+   address_space         = var.management_spoke_vnet_address_space
+}
+
+resource "azurerm_subnet" "admin_subnet" {
+    name                    = "AdminSubnet"
+    resource_group_name     = azurerm_resource_group.rg.name
+    virtual_network_name    = azurerm_virtual_network.management_spoke_vnet.name
+    address_prefixes        = [var.admin_subnet_cidr]
 }
