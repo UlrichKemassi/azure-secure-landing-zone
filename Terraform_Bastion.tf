0e3cd9f16e5b8990c469c402d549a21d0de27d6d
UlrichKemassi <kemassiulrich.itm@gmail.com>
2026-03-15 05:18:10 -0400

    Update Terraform_Bastion.tf

--- Terraform_Bastion.tf
+++ Terraform_Bastion.tf
@@ -1,1 +1,21 @@
+# Public IP for Bastion
+resource "azurerm_public_ip" "bastion_pip" {
+  name                = "bastion-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 
+# Azure Bastion Host
+resource "azurerm_bastion_host" "bastion" { 
+  name                = "hub-bastion" 
+  location            = azurerm_resource_group.rg.location 
+  resource_group_name = azurerm_resource_group.rg.name 
+ 
+  ip_configuration { 
+    name                 = "bastion-ip-config" 
+    subnet_id            = azurerm_subnet.azure_bastion_subnet.id 
+    public_ip_address_id = azurerm_public_ip.bastion_pip.id 
+  } 
+}
