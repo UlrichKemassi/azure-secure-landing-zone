eb5634d23b3e09617747f6d143169944b09d43d5
UlrichKemassi <kemassiulrich.itm@gmail.com>
2026-03-15 05:15:08 -0400

    Update Terraform_Peering.tf

--- Terraform_Peering.tf
+++ Terraform_Peering.tf
@@ -1,1 +1,49 @@
+resource "azurerm_virtual_network_peering" "hub_to_production" {
+  name                      = "hub-to-production"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.hub_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.production_spoke_vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+resource "azurerm_virtual_network_peering" "production_to_hub" {
+  name                      = "production_to_hub"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.production_spoke_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.hub_vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+resource "azurerm_virtual_network_peering" "hub_to_management" {
+  name                      = "hub-to-management"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.hub_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.management_spoke_vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+resource "azurerm_virtual_network_peering" "management_to_hub" {
+  name                      = "management_to_hub"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.management_spoke_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.hub_vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
 
