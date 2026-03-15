8e77e62cb5ce25954b8ff495705b9e3b2cc17d78
UlrichKemassi <kemassiulrich.itm@gmail.com>
2026-03-15 05:19:12 -0400

    Update Terraform_VM.tf

--- Terraform_VM.tf
+++ Terraform_VM.tf
@@ -1,1 +1,42 @@
+# Create NIC
+resource "azurerm_network_interface" "app_vm_nic" {
+  name                = "app-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.app_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "app_vm" {
+  name                = "app-vm"
+  computer_name       = "appvm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2S_v3"
+
+  admin_username                  = "azureuser"
+  admin_password                  = "MySecureVM!2026"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.app_vm_nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
 
